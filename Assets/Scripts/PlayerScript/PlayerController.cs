d85d70ba1a33fab621185d87a323fdc3016702ea
Awxccc <sancturez@gmail.com>
2026-02-04 20:04:15 +0800

    new

---
+++ Assets/Scripts/PlayerScript/PlayerController.cs
@@ -0,0 +1,99 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Cinemachine;
+using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.Search;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static Unity.VisualScripting.Member;
+using static UnityEngine.UIElements.UxmlAttributeDescription;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpHeight = 2f;
+    [SerializeField] private float gravity = -9.8f;
+    [SerializeField] private bool shouldFaceMoveDirection = false;
+    [SerializeField] private Transform yawTarget;
+
+    private CharacterController controller;
+    private Vector2 moveInput;
+    private Vector3 velocity;
+    public bool isAiming;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveDirection = Vector3.zero;
+
+        if(isAiming)
+        {
+            Vector3 forward = transform.forward;
+            Vector3 right = transform.right;
+
+            forward.y = 0;
+            right.y = 0;
+
+            forward.Normalize();
+            right.Normalize();
+            moveDirection = forward * moveInput.y + right * moveInput.x;
+        }
+        else
+        {
+            Vector3 forward = cameraTransform.forward;
+            Vector3 right = cameraTransform.right;
+
+            forward.y = 0;
+            right.y = 0;
+
+            forward.Normalize();
+            right.Normalize();
+            moveDirection = forward * moveInput.y + right * moveInput.x;
+        }
+
+        controller.Move(speed * Time.deltaTime * moveDirection);
+
+        if(isAiming)
+        {
+            Vector3 lookDirection = yawTarget.forward;
+            lookDirection.y = 0;
+
+            if(lookDirection.sqrMagnitude > 0.01f)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(lookDirection);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
+            }
+        }
+        else if (shouldFaceMoveDirection && moveDirection.sqrMagnitude > 0.001f)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, 10f * Time.deltaTime);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity*Time.deltaTime);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if(context.performed && controller.isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+}
--- Assets/Scripts/RagdollController.cs
+++ Assets/Scripts/RagdollController.cs
@@ -1,26 +1,30 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 public class RagdollController : MonoBehaviour
 {
     [SerializeField] private Animator animator;
     private bool isRagdoll = false;
+
     void Awake()
     {
         ActivateRagdoll(isRagdoll);
     }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Keyboard.current != null && Keyboard.current.spaceKey.wasPressedThisFrame)
         {
             animator.enabled = !animator.enabled;
             isRagdoll = !isRagdoll;
             ActivateRagdoll(isRagdoll);
         }
     }
+
     private void ActivateRagdoll(bool active)
     {
-        foreach (Rigidbody rb in
-        GetComponentsInChildren<Rigidbody>())
+        foreach (Rigidbody rb in GetComponentsInChildren<Rigidbody>())
         {
             rb.isKinematic = !active;
         }
